ac531003dd660c57799b5d339aafbd34a15620b0
Astar_007 <1172406997@qq.com>
2019-10-10 21:11:16 +0800

    feat： init linked & add webpack all

--- arithmetic-2019.8.8/29-linked.js
+++ arithmetic-2019.8.8/29-linked.js
@@ -1,2 +1,14 @@
 // 增 删 插入 输出链表 提供索引 
-function linked() {}
+function Linked() {
+    var Node = function (val) {
+        this.val = val;
+        this.next = null;
+    }
+    var length = 0;
+    var head = null;
+}
+Class Linked {
+    Node (val) {
+        this.val = val;
+    }
+}

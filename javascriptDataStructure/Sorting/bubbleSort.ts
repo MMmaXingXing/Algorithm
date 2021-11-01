a27d77cf41dab226ee817f9753c8b79522d20cfd
Hong <1172406997@qq.com>
2021-11-01 10:50:29 +0800

    feat: 冒泡代码实现

---
+++ javascriptDataStructure/Sorting/bubbleSort.ts
@@ -0,0 +1,14 @@
+// 冒泡
+const bubbleSort = (arr: number[]) => {
+  const len = arr.length;
+
+  for (let i = 0; i < len - 1; i++) {
+    for (let j = 0; j < len - 1 - i; j++) {
+      if (arr[j] > arr[j + 1]) {
+        const temp = arr[j + 1];
+        arr[j + 1] = arr[j];
+        arr[j] = temp;
+      }
+    }
+  }
+};

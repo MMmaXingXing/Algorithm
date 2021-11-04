1a06322c729d725a5720dd65f42dd7f1111b012b
Hong <1172406997@qq.com>
2021-11-04 11:56:46 +0800

    feat: 20

--- 20.有效的括号.js
+++ 20.有效的括号.js
@@ -9,5 +9,29 @@
  * @param {string} s
  * @return {boolean
  */
-var isValid = function (s) {};
+var isValid = function (s) {
+  const map = new Map([
+    ["(", ")"],
+    ["[", "]"],
+    ["{", "}"]
+  ]);
+  const arr = s.split("");
+  const len = arr.length;
+  const stack = [];
+  if (len <= 1) return false;
+  for (let i = 0; i < len; i++) {
+    if (map.has(arr[i])) {
+      stack.unshift(arr[i]);
+    } else {
+      const left = stack[0];
+      if (map.get(left) === arr[i]) {
+        stack.shift();
+      } else {
+        return false;
+      }
+    }
+    if (i === len - 1 && stack.length === 0) return true;
+    if (i === len - 1 && stack.length != 0) return false;
+  }
+};
 // @lc code=end

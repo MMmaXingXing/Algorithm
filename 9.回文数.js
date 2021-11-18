122226b905527fabb14b424bcdcc29e998f526c5
Mahongxin <1172406997@qq.com>
2021-11-18 22:06:02 +0800

    feat: 回文数的第二种解法

--- 9.回文数.js
+++ 9.回文数.js
@@ -10,10 +10,15 @@
  * @return {boolean}
  */
 var isPalindrome = function (x) {
-  if (x < 0) return false;
-  const str = x.toString();
-  const list = str.split("");
-  if (str === list.reverse().join("")) return true;
-  return false;
+    if (x < 0 || (x % 10 === 0 && x > 0)) return false;
+    let revertNumber = 0;
+    while (x > revertNumber) {
+        revertNumber = revertNumber * 10 + (x % 10);
+        x = Math.floor(x / 10);
+        console.log(x);
+    }
+
+    if (x === revertNumber || x === Math.floor(revertNumber / 10)) return true;
+    return false;
 };
 // @lc code=end

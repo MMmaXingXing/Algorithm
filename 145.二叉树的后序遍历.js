64b9d76810a1ec75560ce95a272cdfd339d8b6bd
Hong <1172406997@qq.com>
2021-11-23 20:30:26 +0800

    feat: 145.二叉树的后续遍历,96.二叉树的中序遍历

---
+++ 145.二叉树的后序遍历.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=145 lang=javascript
+ *
+ * [145] 二叉树的后序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var postorderTraversal = function (root) {
+  const result = [];
+  const postorder = (root) => {
+    if (!root) return;
+    postorder(root.left);
+    postorder(root.right);
+    result.push(root.val);
+  };
+  postorder(root);
+  return result;
+};
+// @lc code=end
---
+++ 94.二叉树的中序遍历.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=javascript
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  const result = [];
+  const inorder = (root) => {
+    if (!root) return;
+    inorder(root.left);
+    result.push(root.val);
+    inorder(root.right);
+    return result;
+  };
+  inorder(root);
+  return result;
+};
+// @lc code=end

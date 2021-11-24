19e344ac58964bb96b18f1f0cb3f8823e4bc6cb0
Mahongxin <1172406997@qq.com>
2021-11-24 23:06:53 +0800

    feat: 224.基本计算器（空格情况未考虑）

---
+++ 224.基本计算器.js
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=224 lang=javascript
+ *
+ * [224] 基本计算器
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function (s) {
+    let str = s.replace(/\s*/g, "");
+    const stack = [0];
+    for (let i = 0; i < str.length; i++) {
+        if (i === 0) {
+            if (str[i] === "-") {
+                stack.push(`${str[i]}${str[i + 1]}`);
+            } else {
+                stack.push(str[i]);
+            }
+        }
+        switch (str[i]) {
+            case "+":
+                stack.push(str[i + 1]);
+                break;
+            case "-":
+                stack.push(-str[i + 1]);
+                break;
+            case "*":
+                stack.push(stack.pop() * str[i + 1]);
+                break;
+            case "/":
+                stack.push(parseInt(stack.pop() / str[i + 1], 10));
+                break;
+        }
+    }
+    const result = stack.reduce((prev, next) => prev * 1 + next * 1);
+    return result;
+};
+// @lc code=end

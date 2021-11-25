ed6e18e355e5cd95d50055013392fbc5ed0a494e
Mahongxin <1172406997@qq.com>
2021-11-25 22:48:52 +0800

    feat: 202. 快乐数Map实现

--- 202.快乐数.js
+++ 202.快乐数.js
@@ -10,22 +10,22 @@
  * @return {boolean}
  */
 var isHappy = function (n) {
-  const mapper = new Map();
-  let newNum = n;
-  let sum = 0;
-  while (newNum) {
-    if (newNum === 0 && sum !== 1) {
-      if (mapper.has(sum)) {
-        return false;
-      } else {
-        mapper.set(sum, true);
-      }
-      newNum = sum;
+    const mapper = new Map();
+    let newNum = n;
+    let sum = 0;
+    while (newNum || sum !== 1) {
+        if (newNum === 0 && sum !== 1) {
+            if (mapper.has(sum)) {
+                return false;
+            } else {
+                mapper.set(sum, true);
+            }
+            newNum = sum;
+            sum = 0;
+        }
+        sum = Number(sum) + Number(newNum % 10) * Number(newNum % 10);
+        newNum = Math.floor(newNum / 10);
     }
-    newNum = Number(newNum % 10);
-    console.log(newNum);
-    sum += Number(newNum);
-  }
-  return sum === 1;
+    return sum === 1;
 };
 // @lc code=end

54c44f093c9ffba244359fa3cd997364074a5b37
Mahongxin <1172406997@qq.com>
2021-11-27 21:42:28 +0800

    feat: 快乐数链表思维处理调整

--- 202.快乐数-链表思想实现.js
+++ 202.快乐数-链表思想实现.js
@@ -10,21 +10,21 @@
  * @return {boolean}
  */
 var isHappy = function (n) {
-  const getNext = (n) => {
-    let nextNum = 0;
-    while (n) {
-      nextNum += (n % 10) * (n % 10);
-      n /= 10;
-    }
-    return nextNum;
-  };
+    const getNext = (n) => {
+        let nextNum = 0;
+        while (n) {
+            nextNum += (n % 10) * (n % 10);
+            n = Math.floor(n / 10);
+        }
+        return nextNum;
+    };
 
-  let start = n,
-    next = n;
-  do {
-    start = getNext(start);
-    next = getNext(getNext(next));
-  } while (start !== next && next != 1);
-  return q === 1;
+    let start = n,
+        next = getNext(n);
+    do {
+        start = getNext(start);
+        next = getNext(getNext(next));
+    } while (start !== next && next != 1);
+    return next === 1;
 };
 // @lc code=end

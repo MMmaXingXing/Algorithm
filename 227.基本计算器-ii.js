76e92e50079a5cf4f80fd0b7d306febdb346b006
Mahongxin <1172406997@qq.com>
2021-11-28 21:28:43 +0800

    fix: 227.基本计算器2

--- 227.基本计算器-ii.js
+++ 227.基本计算器-ii.js
@@ -10,32 +10,36 @@
  * @return {number}
  */
 var calculate = function (s) {
-  let str = s.replace(/\s*/g, "");
-  const stack = [0];
-  for (let i = 0; i < str.length; i++) {
-    if (i === 0) {
-      if (str[i] === "-") {
-        stack.push(`${str[i]}${str[i + 1]}`);
-      } else {
-        stack.push(str[i]);
-      }
+    let str = s.replace(/\s*/g, "");
+    const stack = [];
+    let newNum = 0;
+    let preSign = "+";
+    const len = str.length;
+
+    for (let i = 0; i < len; i++) {
+        if (!isNaN(Number(str[i]))) {
+            newNum = newNum * 10 + Number(str[i]);
+        }
+        if (isNaN(Number(str[i])) || i === len - 1) {
+            switch (preSign) {
+                case "+":
+                    stack.push(newNum);
+                    break;
+                case "-":
+                    stack.push(-newNum);
+                    break;
+                case "*":
+                    stack.push(stack.pop() * newNum);
+                    break;
+                default:
+                    stack.push(parseInt(stack.pop() / newNum, 10));
+                    break;
+            }
+            preSign = str[i];
+            newNum = 0;
+        }
     }
-    switch (str[i]) {
-      case "+":
-        stack.push(str[i + 1]);
-        break;
-      case "-":
-        stack.push(-str[i + 1]);
-        break;
-      case "*":
-        stack.push(stack.pop() * str[i + 1]);
-        break;
-      case "/":
-        stack.push(parseInt(stack.pop() / str[i + 1], 10));
-        break;
-    }
-  }
-  const result = stack.reduce((prev, next) => prev * 1 + next * 1);
-  return result;
+    const result = stack.reduce((prev, next) => prev * 1 + next * 1);
+    return result;
 };
 // @lc code=end

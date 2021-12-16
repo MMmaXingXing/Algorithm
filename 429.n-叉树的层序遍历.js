7227d77e0fbc5105287eaeaabb47fd3564d2b225
Hong <1172406997@qq.com>
2021-12-16 21:10:03 +0800

    feat: 429.N叉树的层序遍历

--- 429.n-叉树的层序遍历.js
+++ 429.n-叉树的层序遍历.js
@@ -12,12 +12,13 @@
  *    this.children = children;
  * };
  */
-var __levelOrder = (root, ans) => {
-  if (!root.val) return;
-  ans.push(root.val);
+var __levelOrder = (root, res, index) => {
+  if (!root) return;
+  res[index] ? res[index].push(root.val) : (res[index] = [root.val]);
+  let newIndex = index + 1;
   for (let i = 0; i < root.children.length; i++) {
     if (root.children[i]) {
-      __levelOrder(root.children[i], ans);
+      __levelOrder(root.children[i], res, newIndex);
     }
   }
   return;
@@ -28,7 +29,7 @@ var __levelOrder = (root, ans) => {
  */
 var levelOrder = function (root) {
   const ans = [];
-  __levelOrder(root, ans);
+  __levelOrder(root, ans, 0);
   return ans;
 };
 // @lc code=end

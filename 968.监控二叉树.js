b27336f9a4ea765aaff4f7ad06682fc0c2b3ffa5
Mahongxin <1172406997@qq.com>
2021-12-19 18:34:52 +0800

    feat: 968.监控二叉树（问题）

--- 968.监控二叉树.js
+++ 968.监控二叉树.js
@@ -13,9 +13,47 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
+const getDp = (root, dp) => {
+    if (!root) {
+        dp[0][0] = 0;
+        dp[0][1] = 10000;
+        dp[1][0] = 0;
+        dp[1][1] = 10000;
+        return;
+    }
+
+    if (!root.left && !root.rught) {
+        dp[0][0] = 10000;
+        dp[1][0] = 0;
+        dp[1][1] = 1;
+        dp[0][1] = 1;
+        return;
+    }
+
+    const l = [[], []],
+        r = [[], []];
+    getDp(root.left, l);
+    getDp(root.right, r);
+    dp[0][0] = Math.min(
+        Math.min(l[0][1] + r[0][0], l[0][0] + r[0][1]),
+        l[0][1] + r[0][1]
+    );
+    dp[1][0] = Math.min(dp[0][0], l[0][0] + r[0][0]);
+    dp[0][1] =
+        Math.min(
+            Math.min(l[1][0] + r[1][0], l[1][1] + r[1][1]),
+            Math.min(l[1][0] + r[1][1], l[1][1] + r[1][0])
+        ) + 1;
+    dp[1][1] = dp[0][1];
+    return;
+};
 /**
  * @param {TreeNode} root
  * @return {number}
  */
-var minCameraCover = function (root) {};
+var minCameraCover = function (root) {
+    const dp = [[], []];
+    getDp(root, dp);
+    return Math.min(dp[0][1], dp[0][0]);
+};
 // @lc code=end
